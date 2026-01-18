3daa1a542eeb251cf2c65050233ea14bc9760bbe
hehenischal <nischallc56@gmail.com>
2026-01-18 17:49:45 +0545

    debug=false

--- nokhara_job_center/settings.py
+++ nokhara_job_center/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iov!brhx1tz+1$y6engi8(qdun*=1mw=vojr0*4ooa#lezio#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hehenischal.pythonanywhere.com', ]
 
 
 # Application definition
